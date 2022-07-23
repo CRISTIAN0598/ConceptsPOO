bff2b3933560f69df29237c14d6ffbda0a04a974
cristian@somosmakers.co <cristian@somosmakers.co>
2022-07-23 17:39:48 -0500

    Finish date class

---
+++ POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace POOConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("POO Concepts!");
+            Console.WriteLine("==============");
+
+
+            try
+            {
+                Console.WriteLine(new Date(2024,2,29));
+                Console.WriteLine(new Date(1974, 11, 31));
+                
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
