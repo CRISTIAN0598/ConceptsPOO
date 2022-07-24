41d28732f8e92d8101455b6584b29d332eb071f0
cristian@somosmakers.co <cristian@somosmakers.co>
2022-07-24 16:48:06 -0500

    Employee,SalaryEmployee & CommissionEmployee finish

--- POOConcepts/POOConcepts/Program.cs
+++ POOConcepts/POOConcepts/Program.cs
@@ -9,18 +9,34 @@ namespace POOConcepts
             Console.WriteLine("POO Concepts!");
             Console.WriteLine("==============");
 
-
-            try
+            Employee employee1 = new SalaryEmployee()
             {
-                Console.WriteLine(new Date(2024,2,29));
-                Console.WriteLine(new Date(1974, 11, 31));
+                Id = 1010,
+                FirstName = "Sandra",
+                LastName = "Morales",
+                BirthDate = new Date(1990, 5, 23),
+                HiringDate = new Date(2022, 1, 15),
+                IsActive = true,
+                Salary=1815453.45M
+            };
+
+            Employee employeeCommision = new CommisssionEmployee()
+            {
+                Id = 2020,
+                FirstName = "Juan",
+                LastName = "Perez",
+                BirthDate = new Date(1990, 5, 23),
+                HiringDate = new Date(2022, 1, 15),
+                IsActive = true,
+                Sales= 320000000M,
+                CommissionPercentaje=0.03F
                 
-            }
-            catch (Exception ex)
-            {
+            };
 
-                Console.WriteLine(ex.Message);
-            }
+            Console.WriteLine(employee1);
+            Console.WriteLine(employeeCommision);
+
+
         }
     }
 }
